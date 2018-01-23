fb1f056399897d7818232fb047b3da7eddad4ffe
Alexander <32452033+avbor@users.noreply.github.com>
2018-01-23 17:30:37 +0300

    Added '-Proxy' parameter and some other twiks

--- PowerShell/Get-WbsApl-HotfixInfo.ps1
+++ PowerShell/Get-WbsApl-HotfixInfo.ps1
@@ -1,14 +1,27 @@
 ﻿# Get-WbsApl-HotfixInfo.ps1
 
+Param (
+    [Parameter(Position=0, Mandatory = $false)][string]$Proxy
+)
+
 $baseUri = "http://appliancehotfix.websense.com/download/hotfixes/index.json"
 
-$getBase = Invoke-RestMethod -Method Get -Uri $baseUri
+$OpProxy = @{}
+if ($proxy) {
+    while ($proxy -notlike "http*") {
+        $proxy = Read-Host "Proxy address (http://proxy:8080)"
+    }
+    $OpProxy['Proxy'] = $proxy
+}
+
+try {$getBase = Invoke-RestMethod -Method Get -Uri $baseUri -Headers @{"Cache-Control"="no-cache"} @OpProxy}
+catch {Write-Host $_.Exception.Message; Exit}
 $allUri = $getBase.PSObject.Properties | ForEach-Object {$_.Value}
 $aVers = (($getBase.PSObject.Properties | ForEach-Object {$_.value.PSObject.Properties} | Select-Object Name -Unique).Name) | Sort-Object
 $hfs = @()
 $App = @()
 $Web = @()
-$Proxy = @()
+$Wcg = @()
 $Email = @()
 $Other = @()
 
@@ -24,22 +37,26 @@ if (!$userChoice) {$ver = $aVers[$lastVer-1]}
 else {$ver = $aVers[$userChoice-1]}
 
 foreach ($uri in $allUri.$ver) {
-    if ($uri) {$hfs += Invoke-RestMethod -Method Get -Uri $uri}
+    if ($uri) {
+        try {$hfs += Invoke-RestMethod -Method Get -Uri $uri -Headers @{"Cache-Control"="no-cache"} @OpProxy}
+        catch {Write-Host $_.Exception.Message; Exit}
+    }
 }
 
 foreach ($hf in $hfs) {
-    if ($hf.id -like "App-*") {$App += $hf}
-    elseif ($hf.id -like "Web-*") {$Web += $hf}
-    elseif ($hf.id -like "Proxy-*") {$Proxy += $hf}
-    elseif ($hf.id -like "Email-*") {$Email += $hf}
+    if ($hf.module -eq "app") {$App += $hf}
+    elseif ($hf.module -eq "web") {$Web += $hf}
+    elseif ($hf.module -eq "proxy") {$Wcg += $hf}
+    elseif ($hf.module -eq "email") {$Email += $hf}
     else {$Other += $hf}
+    $hf = $null
 }
 
 function PrintHFInfo ($fix) {
     Write-Host -ForegroundColor Green $fix.id
     Write-Host -ForegroundColor White $fix.description
-    if ($hf.url) {Write-Host "Download:" $fix.url}
-    if ($hf.release_note_url) {Write-Host "Release notes:" $fix.release_note_url}
+    if ($fix.url) {Write-Host "Download:" $fix.url}
+    if ($fix.release_note_url) {Write-Host "Release notes:" $fix.release_note_url}
     Write-Host
 }
 
@@ -50,28 +67,33 @@ if ($App) {
     foreach ($hf in $App) {
         PrintHFInfo -fix $hf
     }
+    $hf = $null
 }
 if ($Web) {
     Write-Host -ForegroundColor Yellow "`nFor Filtering Services ["$Web.Count"]:`n" -Separator ""
     foreach ($hf in $Web) {
         PrintHFInfo -fix $hf
     }
+    $hf = $null
 }
-if ($Proxy) {
-    Write-Host -ForegroundColor Yellow "`nFor Content Gateway ["$Proxy.Count"]:`n" -Separator ""
-    foreach ($hf in $Proxy) {
+if ($Wcg) {
+    Write-Host -ForegroundColor Yellow "`nFor Content Gateway ["$Wcg.Count"]:`n" -Separator ""
+    foreach ($hf in $Wcg) {
         PrintHFInfo -fix $hf
     }
+    $hf = $null
 }
 if ($Email) {
     Write-Host -ForegroundColor Yellow "`nFor Email Security Gateway ["$Email.Count"]:`n" -Separator ""
     foreach ($hf in $Email) {
         PrintHFInfo -fix $hf
     }
+    $hf = $null
 }
 if ($Other) {
     Write-Host -ForegroundColor Yellow "`nFor Other components ["$Other.Count"]:`n" -Separator ""
     foreach ($hf in $Other) {
         PrintHFInfo -fix $hf
     }
+    $hf = $null
 }
